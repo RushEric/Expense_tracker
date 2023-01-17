f826d428d8799d370f13ad3bf4ee870c78d1c221
RushEric <59781234+RushEric@users.noreply.github.com>
2023-01-16 21:10:26 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import { Header } from './component/Header';
+import { Balance } from './component/Balance';
+import { IncomeExpenses } from './component/IncomeExpenses';
+import { TransactionList } from './component/TransactionList';
+import { AddTransaction } from './component/AddTransaction';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className = "container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </div>
+  );
+}
+
+export default App;
